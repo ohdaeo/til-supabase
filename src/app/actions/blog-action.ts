6e697c31aec29085cf4d355a5d8a7383145a366a
ohdaeo <assa01389@gmail.com>
2025-03-28 14:40:37 +0900

    tiptap-연동

---
+++ src/app/actions/blog-action.ts
@@ -0,0 +1,109 @@
+"use server";
+
+import { createServerSideClient } from "@/lib/supabase/server";
+import { Database } from "@/types/types_db";
+export type TodosRow = Database["public"]["Tables"]["blog"]["Row"];
+export type TodosRowInsert = Database["public"]["Tables"]["blog"]["Insert"];
+export type TodosRowUpdate = Database["public"]["Tables"]["blog"]["Update"];
+
+// Create 기능
+export async function createBlog(blog: TodosRowInsert) {
+  const supabase = await createServerSideClient();
+  const { data, error, status } = await supabase
+    .from("blog")
+    .insert([
+      {
+        title: blog.title,
+        content: blog.content,
+      },
+    ])
+    .select()
+    .single();
+
+  return { data, error, status };
+}
+// Read 기능
+export async function getBlogs() {
+  console.log("getblogs =============");
+  const supabase = await createServerSideClient();
+  const { data, error, status } = await supabase
+    .from("blog")
+    .select("*")
+    .order("id", { ascending: false });
+  return { data, error, status } as {
+    data: TodosRow[] | null;
+    error: Error | null;
+    status: number;
+  };
+}
+
+// Read 기능 id 한개
+export async function getBlogId(id: number) {
+  const supabase = await createServerSideClient();
+  const { data, error, status } = await supabase
+    .from("blog")
+    .select()
+    .eq("id", id)
+    .single();
+  return { data, error, status } as {
+    data: TodosRow | null;
+    error: Error | null;
+    status: number;
+  };
+}
+
+// Update 기능 id 한개
+export async function updateBlogId(id: number, title: string, content: string) {
+  const supabase = await createServerSideClient();
+
+  const { data, error, status } = await supabase
+    .from("blog")
+    .update({ title: title, content: content })
+    .eq("id", id)
+    .select()
+    .single();
+
+  return { data, error, status } as {
+    data: TodosRow | null;
+    error: Error | null;
+    status: number;
+  };
+}
+// Title 업데이트 함수
+
+// Update 기능 id 한개
+export async function updateTodoIdTitle(
+  id: number,
+  title: string,
+  startDate: Date | undefined,
+  endDate: Date | undefined
+) {
+  const supabase = await createServerSideClient();
+
+  const { data, error, status } = await supabase
+    .from("todos")
+    .update({
+      title: title,
+      start_date: startDate?.toISOString(),
+      end_date: endDate?.toISOString(),
+    })
+    .eq("id", id)
+    .select()
+    .single();
+
+  return { data, error, status } as {
+    data: TodosRow | null;
+    error: Error | null;
+    status: number;
+  };
+}
+// Row 삭제 기능
+export async function deleteBlog(id: number) {
+  const supabase = await createServerSideClient();
+  const { error, status } = await supabase.from("blog").delete().eq("id", id);
+
+  return { error, status } as {
+    error: Error | null;
+    status: number;
+  };
+}
--- src/components/editor/create-editor.tsx
+++ src/components/editor/create-editor.tsx
@@ -2,6 +2,9 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+// css
+import styles from "@/components/editor/editor.module.css";
+
 // extension : 내용 정렬
 import TextAlign from "@tiptap/extension-text-align";
 // extension : color
@@ -17,14 +20,22 @@ import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
 
 import Toolbar from "./toolbar";
+// Sahdcn
+import { Button } from "@/components/ui/button";
+import { createBlog } from "@/app/actions/blog-action";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export const CreateEditor = () => {
+  // 내용
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
   // 배경색
   const lowlight = createLowlight(common);
   const CustomHighlight = Highlight.configure({
     multicolor: true,
   });
-
+  // 에디터
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -44,17 +55,53 @@ export const CreateEditor = () => {
           class: "cursor-pointer text-blue-500 hover:underline",
         },
       }),
-
       Image,
     ],
-    content: "<p>안녕하세요.</p>",
+    content: content, // 초기값
+    // 내용 갱신시 실행
+    onUpdate({ editor }) {
+      // 내용읽기
+      setContent(editor.getHTML());
+    },
   });
+
+  const onSubmit = async () => {
+    const { data, error, status } = await createBlog({
+      title,
+      content,
+    });
+    if (!title || !content) {
+      toast.error("내용과 제목을 입력해주세요.");
+      return;
+    }
+
+    toast.success("글이 성공적으로 등록되었습니다.");
+    // 내용 초기화
+    setTitle("");
+    setContent("");
+  };
+
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-[95%] flex flex-col bg-white my-3 p-3">
       <h3>블로그 작성하기</h3>
-      <div>
-        {editor && <Toolbar editor={editor} />}
-        <EditorContent editor={editor} />
+      <div className="w-full flex-col items-center justify-center">
+        <div className="w-full my-2">
+          <input
+            className="w-full p-2 border-2 border-gray-300 rounded-md"
+            placeholder="제목을 입력해주세요"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.editor}>
+          {editor && <Toolbar editor={editor} />}
+          <EditorContent editor={editor} />
+        </div>
+        <div className="flex w-full items-center justify-center p-2">
+          <Button type="button" className="px-4 py-2" onClick={onSubmit}>
+            Add Blog
+          </Button>
+        </div>
       </div>
     </div>
   );
